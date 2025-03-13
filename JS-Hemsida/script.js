ec47cdaaf75833648e8b0d2fe1c4faabed3eca24
Victor-Johansson <victor.johansson@elev.ga.ntig.se>
2025-03-13 08:45:09 +0100

    Gjorde en lista istället för en  lång if-sats

--- JS-Hemsida/script.js
+++ JS-Hemsida/script.js
@@ -10,21 +10,11 @@ function resetNumberOfClicks() {
     document.getElementById('counter').innerText = count
 }
 
+let lista = [10, 100, 500, 1000, 2500, 5000, 10000] 
+
 function acheivement() {
-    if (count >= 10 && count <= 10) {
-        alert("You have clicked 10 times!")
-    } else if (count >= 100 && count <= 100) {
-        alert("You have clicked 100 times!")
-    } else if (count >= 500 && count <= 500) {
-        alert("You have clicked 500 times!")
-    } else if (count >= 1000 && count <= 1000) {
-        alert("You have clicked 1000 times!")
-    } else if (count >= 2500 && count <= 2500) {
-        alert("You have clicked 2500 times!")
-    } else if (count >= 5000 && count <= 5000) {
-        alert("You have clicked 5000 times!")
-    } else if (count >= 10000 && count <= 10000) {
-        alert("You have clicked 10000 times!")
+    if (lista.includes(count)) {
+        alert('Grattis! Du har klickat ' + count + ' gånger!')
     }
 }
 
